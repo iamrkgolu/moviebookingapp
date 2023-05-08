24eb98648752e615876539f55b99ac1fd2418cbd
iamrkgolu <iamrkgolu@gmail.com>
2023-05-08 14:08:37 +0530

    Adding Test cases

--- src/test/java/com/moviebookingapp/MoviebookingappApplicationTests.java
+++ src/test/java/com/moviebookingapp/MoviebookingappApplicationTests.java
@@ -3,11 +3,14 @@ package com.moviebookingapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.moviebookingapp.model.Movie;
+
 @SpringBootTest
 class MoviebookingappApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
 	}
 
 }
